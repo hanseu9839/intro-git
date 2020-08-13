9422fd6138e41280c517eb5a8716632135d5ede2
hanseu9839 <hanseu9839@gmail.com>
2020-08-13 20:24:29 +0900

    read.md

--- TBC9.1/main.c
+++ TBC9.1/main.c
@@ -1,28 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h> // strlen()
+#include<stdbool.h>
 
 #define WIDTH 20
 #define NAME "Seung-Gyun Han"
 #define ADDRESS "Inchoen, Korea"
-void print_var(int n_stars);
-int main(){
-	print_var(WIDTH);
 
-	printf("   %s\n", NAME);
+void print_multiple_char(char c,int n_stars, bool print_newline);
+void print_centered_str(char str[]);
 
-	printf("   %s\n", ADDRESS);
+int main()
+{
+	print_multiple_char('*',WIDTH, true);
 
-	print_var(WIDTH);
+	print_centered_str(NAME);
+	print_centered_str(ADDRESS);
+	print_centered_str("l love you");
+
+	print_multiple_char('*',WIDTH, false);
+
 	return 0;
 }
 
-void print_var(int n_stars)
+//void print_multiple_char(char c,int n_stars, bool print_newline)
+//{
+//	for (int i = 0; i < n_stars; i++)
+//		printf("%c", c);
+//
+//	if (print_newline)
+//		printf("\n");
+//	
+//}
+void print_centered_str(char str[])
 {
-	for (int i = 0; i < n_stars; i++)
-	{
-		printf("*");
-	}
-	printf("\n");
+	int n_blanks = 0;
+	n_blanks = (WIDTH - strlen(str)) / 2;
+	print_multiple_char(' ', n_blanks, false);
+	printf("%s\n", str);
 }
-
 
